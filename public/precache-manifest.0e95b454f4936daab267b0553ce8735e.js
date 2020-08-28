4eb158b868971d669bb8d0d55a94d41f7ba214b1
Devan Wheeler <devan@DESKTOP-1MO517M.localdomain>
2020-08-28 14:49:04 -0700

    https switch

---
+++ public/precache-manifest.0e95b454f4936daab267b0553ce8735e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4af49b218f5a63c28841bb4783354c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e5e3888047368cea779",
+    "url": "/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "0e5e3888047368cea779",
+    "url": "/static/js/2.2328e7ca.chunk.js"
+  },
+  {
+    "revision": "757fe2fc6bae9340dd8c689c5ee0ea0f",
+    "url": "/static/js/2.2328e7ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f904eeabe26b504cf9",
+    "url": "/static/js/main.d6dc3edd.chunk.js"
+  },
+  {
+    "revision": "8aba14c5515e5362888a",
+    "url": "/static/js/runtime-main.66cf0057.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,10 +1,37 @@
-const express = require("express");
+/*
+
+This module creates an HTTPS web server and serves static content
+from a specified directory on a specified port.
+
+To generate a new cert:
+
+  openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365
+
+To remove the passphrase requirement:
+
+  openssl rsa -in key.pem -out newkey.pem && mv newkey.pem key.pem
+
+Or just include the "passphrase" option when configuring the HTTPS server.
+
+Sources:
+- http://blog.mgechev.com/2014/02/19/create-https-tls-ssl-application-with-express-nodejs/
+- https://expressjs.com/en/starter/static-files.html
+
+*/
+
+const fs = require('fs');
+const https = require('https');
+const express = require('express');
+
 const app = express();
-const port = 4000;
-const http = require("http");
-const server = http.createServer(app);
-//express stuff
 app.use(express.static(__dirname + "/public"));
 
-server.listen(port, ()=>console.log(`Server is running on port ${port}`));
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/archive/webrtctestkit.tk/privkey1.pem', 'utf8'),
+  cert: fs.readFileSync('/etc/letsencrypt/archive/webrtctestkit.tk/cert1.pem', 'utf8'),
+  passphrase: process.env.HTTPS_PASSPHRASE || '',
+  port:443
+};
+const server = https.createServer(options, app);
 
+server.listen(process.env.SERVER_PORT || 443,()=>console.log(`Server is running on port ${port}`));
